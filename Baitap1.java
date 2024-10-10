4021c863bfc12fc31367f59bb1e761d95431f1d6
Aka2911 <tkhai098@gmail.com>
2024-10-10 22:56:27 +0700

    dmm

---
+++ Baitap1.java
@@ -0,0 +1,17 @@
+package MYPROJECT;
+
+public class Baitap1 {
+    public static void main(String[] args) {
+        //In mot chuoi
+        System.out.println("Toi dang lam quen voi ngon ngu lap trinh Java!");
+        // Khai bao bien gan du lieu va in ra man hinh
+        String str = "I love vanlang univercity";
+        // In ra gia tri cuar bien str
+        System.out.println(str);
+        // Noi chuoi va in ra man hinh
+        String str1 = "Dai hoc van lang";
+        String str2 = "dao duc - y chi - sang tao";
+        System.out.println(str1 + " "+str2);
+    
+    } 
+}
